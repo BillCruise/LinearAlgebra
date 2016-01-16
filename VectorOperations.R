a80c9064802e0156cd85a37c2c0b65eb6f79ce10
Bill Cruise <cruise02@gmail.com>
2016-01-16 08:23:04 -0500

    Added dot product and vector norm examples.

--- VectorOperations.R
+++ VectorOperations.R
@@ -1,7 +1,7 @@
 # Basic vector arithmetic operations are performed member-by-member.
 
-a = c(1, 3, 5, 7)
-b = c(1, 2, 4, 8)
+a <- c(1, 3, 5, 7)
+b <- c(1, 2, 4, 8)
 
 # Vector addition
 a + b
@@ -19,7 +19,7 @@ a / b
 -a
 
 # Scalar/Vector operations
-s = 2
+s <- 2
 a + s
 a - s
 a * s
@@ -29,6 +29,29 @@ a / s
 # If two vectors are of uneven length, elements from the shorter vector 
 # will be recycled to match the length of the longer vector.
 
-u = c(10, 20, 30)
-v = c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+u <- c(10, 20, 30)
+v <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
 u + v
+
+
+# Dot Product
+u <- c(1, -2, 3)
+v <- c(4, 5, -1)
+w <- c(2, 7, 4)
+u %*% v
+u %*% w
+v %*% w
+
+# If the dot product of two vectors is 0, the two vectors are ORTHOGONAL (or PERPENDICULAR).
+
+
+# Vector Norm
+# The NORM (or length) of a vector is the Euclidean length of the vector.
+# The norm of a vector u is defined as the nonnegative square root of u * u.
+norm_vec <- function(u) sqrt(sum(u^2))
+
+u <- c(1, -2, -4, 5, 3)
+norm_vec(u)
+norm(u, "F")
+
+v <- c()
