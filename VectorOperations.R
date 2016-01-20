ffde15ea7a85be7e115257868269c21143b0bbfb
Bill Cruise <cruise02@gmail.com>
2016-01-20 17:17:17 -0500

    Added a section on vector equality.

--- VectorOperations.R
+++ VectorOperations.R
@@ -18,6 +18,29 @@ a / b
 # Vector negation
 -a
 
+# Vector equality
+# Use either all() combined with ==, or identical() to test vector equality.
+u1 <- c(1, 2, 3)
+u2 <- c(2, 3, 1)
+u3 <- c(1, 3, 2)
+u4 <- c(2, 3, 1)
+
+all(u1 == u2) # FALSE
+all(u2 == u3) # FALSE
+all(u2 == u4) # TRUE
+
+identical(u1, u2)
+identical(u2, u3)
+identical(u2, u4)
+
+# use which() to find indices of elements that are equal or not equal.
+which(u1 == u2)
+which(u1 != u2)
+
+which(u1 == u3)
+which(u2 == u3)
+which(u2 == u4)
+
 # Scalar/Vector operations
 s <- 2
 a + s
