5bcb204beec0144ea705fecbd03c75cdfc905939
Bill Cruise <cruise02@gmail.com>
2016-01-23 12:19:19 -0500

    Started section 1.5

---
+++ SolutionSetsOfLinearEquations.R
@@ -0,0 +1,10 @@
+# Linear Algebra and its applications 5th edition
+# Section - 1.5 Solution Sets of Linear Systems
+
+# Definitions
+
+# A system of linear equations is said to be HOMOGENEOUS if it can be written in the form
+# Ax = 0, where A is an mxn matrix, and 0 is the zero vector in Rm. A homogeneous system
+# always has at least one solution, namely, x = 0 (the zero vector in Rn). This zero
+# solution is usually called the TRIVIAL SOLUTION. For a given equation Ax = 0, the
+# important question is whether there exists a NONTRIVIAL solution.
