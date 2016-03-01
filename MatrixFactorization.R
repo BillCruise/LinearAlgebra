1f7d7f98bf0366abd4675f55897ca04e4fc79ec8
Bill Cruise <cruise02@gmail.com>
2016-02-29 21:54:43 -0500

    LU matrix factorization

---
+++ MatrixFactorization.R
@@ -0,0 +1,13 @@
+# A FACTORIZATION of a matrix X is an equation that 
+# expresses A as a product of two or more matrices.
+
+library(matrixcalc)
+
+A <- matrix(data=c(3, -7, -2, 2,
+                   -3, 5, 1, 0,
+                   6, -4, 0, -5,
+                   -9, 5, -5, 12), nrow=4, ncol=4, byrow=TRUE)
+
+luA <- lu.decomposition(A)
+L <- luA$L
+U <- luA$U
