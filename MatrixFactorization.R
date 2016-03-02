44073c390b9025917d23eef8fc1f3b5dd8cb24db
Bill Cruise <cruise02@gmail.com>
2016-03-02 18:03:35 -0500

    Added heading information

--- MatrixFactorization.R
+++ MatrixFactorization.R
@@ -1,3 +1,6 @@
+# Linear Algebra and its applications 5th edition
+# Section 2.5 - Matrix Factorizations
+
 # A FACTORIZATION of a matrix X is an equation that 
 # expresses A as a product of two or more matrices.
 
