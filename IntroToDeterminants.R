28646fd56b8c24cad8d8fd7b2cd43b9fdedf3f86
Bill Cruise <cruise02@gmail.com>
2016-03-12 12:08:09 -0500

    Added exercise solutions.

--- IntroToDeterminants.R
+++ IntroToDeterminants.R
@@ -5,7 +5,7 @@
 
 # The DETERMINANT of a square matrix is the number defined inductively by a 
 # cofactor expansion along the first row of A. Also, (-1)^r times the product 
-# of the diagonal entries in any echelon from U obtained from A by 
+# of the diagonal entries in any echelon form U obtained from A by 
 # row replacements and row interchanges (but no scaling operations).
 
 # COFACTOR EXPANSION is a formula for det A using cofactors associated with 
@@ -50,3 +50,36 @@ A <- matrix(data=c(2, 3, -3,
                    4, 0, 3,
                    6, 1, 5), nrow=3, ncol=3, byrow=TRUE)
 detA <- det(A)
+
+# Exercise 7
+A <- matrix(data=c(4, 3, 0,
+                   6, 5, 2,
+                   9, 7, 3), nrow=3, ncol=3, byrow=TRUE)
+detA <- det(A)
+
+# Exercise 9
+A <- matrix(data=c(4, 0, 0, 5,
+                   1, 7, 2, -5,
+                   3, 0, 0, 0,
+                   8, 3, 1, 7), nrow=4, ncol=4, byrow=TRUE)
+detA <- det(A)
+
+
+# Exercise 11
+A <- matrix(data=c(3, 5, -6, 4,
+                   0, -2, 3, -3,
+                   0, 0, 1, 5,
+                   0, 0, 0, 3), nrow=4, ncol=4, byrow=TRUE)
+detA <- det(A)
+
+
+# Exercise 13
+A <- matrix(data=c(4, 0, -7, 3, -5,
+                   0, 0, 2, 0, 0,
+                   7, 3, -6, 4, -8,
+                   5, 0, 5, 2, -3,
+                   0, 0, 9, -1, 2), nrow=5, ncol=5, byrow=TRUE)
+detA <- det(A)
+
+
+
