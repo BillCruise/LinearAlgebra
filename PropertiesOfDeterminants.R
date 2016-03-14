12d9a485ef69ccc77093da216686ad54b2f8f20f
Bill Cruise <cruise02@gmail.com>
2016-03-14 09:48:52 -0400

    Exercises from Properties of Determiniants

---
+++ PropertiesOfDeterminants.R
@@ -0,0 +1,24 @@
+# Linear Algebra and its applications 5th edition
+# Section 3.2 - Properties of Determinants
+
+
+# Exercise 5
+A <- matrix(data=c(1, 5, -4,
+                   -1, -4, 5,
+                   -2, -8, 7), nrow=3, ncol=3, byrow=TRUE)
+detA <- det(A)
+
+
+# Exercise 6
+A <- matrix(data=c(3, 3, -3,
+                   3, 4, -4,
+                   2, -3, -5), nrow=3, ncol=3, byrow=TRUE)
+detA <- det(A)
+
+
+# Exercise 7
+A <- matrix(data=c(1, 3, 0, 2,
+                   -2, -5, 7, 4,
+                   3, 5, 2, 1,
+                   1, -1, 2, -3), nrow=4, ncol=4, byrow=TRUE)
+detA <- det(A)
